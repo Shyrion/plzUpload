86dd856f4716cf847f1048690ec626e6edef4f73
Jeremy Gabriele <jeremy.gabriele@gmail.com>
2014-04-29 00:32:21 +0200

    Upload codes generation update

--- app/models/UploadCode.js
+++ app/models/UploadCode.js
@@ -1,6 +1,7 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Upload = require('./Upload');
 
 var UploadCodeSchema = new Schema({
 	name: String
@@ -35,6 +36,8 @@ var syllables = ['a', 'za', 'ra', 'ta', 'ya', 'pa', 'qa', 'sa', 'da', 'fa', 'ga'
 UploadCode.fillDB = function fillDB(nbDigits) {
 	// TODO: be able to get codes of length 3, 5, 7, 9 when requesting 9 (for example)
 
+	console.log("Going to generate code of length ", nbDigits);
+
 	// Recursively create random word from syllabaire
 	function getWordRecursive(word, count, callback) {
 		if (count <= 0) {
@@ -49,18 +52,34 @@ UploadCode.fillDB = function fillDB(nbDigits) {
 
 	// Launch the algo
 	getWordRecursive('', nbDigits, function(word) {
-		UploadCode.findOne({name: word}, function(err, result) {
+		// Find if an upload is already using that name
+		Upload.findOne({code: word}, function(err, result) {
 			if (err) {
-				console.error(err);
+				console.error("Error while finding upload",err);
 			} else {
-				if (result) return; // already in DB
-
-				var upCode = new UploadCode({name: word});
-				upCode.save(function(err, uploadCode) {
+				if (result) { // already used in Upload
+					console.log("Code ", word, "already used for upload.");
+					return;
+				}
+				// Check if we don't already have it in the DB
+				// (the name is not the private key, need to check manually :/)
+				UploadCode.findOne({name: word}, function(err, result) {
 					if (err) {
-						console.error(err);
+						console.error("Error while finding code",err);
 					} else {
-						console.log('[Generated code: ]' + upCode.name);
+						if (result) { // already in DB
+							console.log("Code ", word, "already waiting for upload.");
+							return;
+						}
+
+						var upCode = new UploadCode({name: word});
+						upCode.save(function(err, uploadCode) {
+							if (err) {
+								console.error("Error while saving", err);
+							} else {
+								console.log('[Generated code: ' + upCode.name + ']');
+							}
+						});
 					}
 				});
 			}
